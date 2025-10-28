6cc18d45ab06e47fcc9631a5811219c5ae884485
HaechanShin <gocks114@gmail.com>
2025-10-28 06:01:36 +0100

    Create create_folders.bat

---
+++ create_folders.bat
@@ -0,0 +1,9 @@
+mkdir data models src mlops streaming api notebooks tests docs scripts config
+mkdir data\raw data\processed data\synthetic
+mkdir models\anomaly_detection models\graph_analysis models\time_series models\ensemble  
+mkdir src\data_processing src\feature_engineering src\training src\inference src\utils
+mkdir mlops\pipelines mlops\deployment mlops\monitoring mlops\experiments
+mkdir streaming\kafka_setup streaming\producers streaming\consumers streaming\processors
+mkdir api\routes
+mkdir tests
+pause
